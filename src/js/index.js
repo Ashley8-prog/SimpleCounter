8fcae8e43a5d583e9bc4397d43a059628d641379
Ashley Molina <131078848+Ashley8-prog@users.noreply.github.com>
2023-07-27 04:34:47 +0000

    Cambios26/7

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,30 @@
-//import react into the bundle
+
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
-
-//import your own components
 import Home from "./component/home.jsx";
+import SecondsCounter from "./component/secondsCounter";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+
+// render your react application
+let contador = 0;
+let contador2 = 0;
+let contador3 = 0;
+let contador4 = 0;
+let contador5 = 0;
+let contador6 = 0;
+
+setInterval(() => {
+    ReactDOM.render(
+        <SecondsCounter seconds={contador} seconds2={contador2} seconds3={contador3} seconds4={contador4} seconds5={contador5} seconds6= {contador6}/>,
+        document.querySelector("#app")
+    );
+
+    contador = contador + 1;
+    contador2 = contador2 + 1;
+    contador3 = contador3 + 1;
+    contador4 = contador4 + 1;
+    contador5 = contador5 + 1;
+    contador6 = contador6 + 1;
+},1000);
